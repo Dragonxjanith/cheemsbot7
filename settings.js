17e3951f96be707a7b16eee636622818e3801deb
Queen NIlu <101912503+Dragonxjanith@users.noreply.github.com>
2023-06-06 08:46:26 +0530

    fff

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
+global.owner = ['94767438882'] //ur owner number
+global.ownernomer = "94767438882" //ur owner number2
 global.ownername = "🦄Dream Guy Xeon" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
@@ -19,9 +19,9 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = "Cheems Bot MD V7"
-global.ownernumber = '916909137213'
+global.ownernumber = '94767438882'
 global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.ownerNumber = ["94767438882@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/ecl-JDVDz3E"
 global.wagc = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
